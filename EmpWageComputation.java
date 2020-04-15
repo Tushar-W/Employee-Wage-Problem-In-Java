d63c6757c0ea546d41f63ad1ce07c7bcdf2639e0
Tushar-W <twankhede95@gmail.com>
2020-04-15 17:33:20 +0530

    ADDED:feature for calculate employee daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,12 +3,18 @@ public class EmpWageComputation {
 	public static void main(String[] args) {
 		// CONSTANTS
 		int IS_FULL_TIME = 1;
+		int EMP_WAGE_PER_HOUR = 20;
+		// VARIABLES
+		int empHrs=0;
+		int empWage=0;
 		// COMPUTATION
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee Is Present");
+			empHrs=8;
 		else
-			System.out.println("Employee Is Abscent");
+			empHrs=0;
+		empWage = empHrs * EMP_WAGE_PER_HOUR;
+		System.out.println("Employee Wage: "+ empWage);
 	}
 }
 
